d5cd95b7599b76ced02c193a69fb8255555377f2
igralex <igralex1@gmail.com>
2018-11-28 18:53:59 +0300

    first commit

---
+++ SimpleApplication/SimpleApplication/Models/GuestResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SimpleApplication.Models
+{
+    public class GuestResponse
+    {
+      [Required(ErrorMessage="Please enter your name")]
+      public string Name { get; set; }
+      [Required(ErrorMessage = "Please enter your emial")]
+      public string Email { get; set; }
+      [Required(ErrorMessage="Please enter your phone number")]
+      public string Phone { get; set; }
+      [Required(ErrorMessage="Please give answer?")]
+      public bool WillAttend { get; set; }
+    }
+}
